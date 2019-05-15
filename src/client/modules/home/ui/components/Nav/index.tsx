8cc33eff104f5a954fcdeebbf1226ddb6a38b16b
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-05-15 19:23:05 -0300

    Set logout functionality on client-side

--- src/client/modules/home/ui/components/Nav/index.tsx
+++ src/client/modules/home/ui/components/Nav/index.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+import { Mutation } from "react-apollo";
+import useOnClickOutside from "use-onclickoutside";
+import { logoutUser } from "../../../../../../server/schema/graphql/Mutations.graphql";
+import { getUserIdFromSession } from "../../../../../../server/schema/graphql/Queries.graphql";
+import { LogoutUserMutation } from "../../../../../__types__/typeDefs";
 import "./main.scss";
 
 interface Props {
@@ -6,9 +11,34 @@ interface Props {
 }
 
 const Nav: React.FunctionComponent<Props> = props => {
+  const [dropDown, setDropDown] = React.useState(false);
+  const dropDownMenu = React.useRef();
+  const hideDropDown = () => {
+    return setDropDown(false);
+  };
+  useOnClickOutside(dropDownMenu, hideDropDown);
+
   return (
     <nav>
-      <span>{props.username}</span>
+      <span onMouseEnter={() => setDropDown(true)}>{props.username}</span>
+      {dropDown ? (
+        <div className="nav-dropdown-menu" ref={dropDownMenu}>
+          <Mutation<LogoutUserMutation>
+            mutation={logoutUser}
+            refetchQueries={[{ query: getUserIdFromSession }]}
+          >
+            {mutate => (
+              <span
+                onClick={async () => {
+                  mutate();
+                }}
+              >
+                Logout
+              </span>
+            )}
+          </Mutation>
+        </div>
+      ) : null}
     </nav>
   );
 };
