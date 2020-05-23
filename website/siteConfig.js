3249ae0c8197fd42a2db3cf63aff113f64a13e98
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-05-23 12:57:06 -0300

    chore(wip): docs organization

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "george-docs",
+  projectName: "george",
   organizationName: "marcelovicentegc",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
